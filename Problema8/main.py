5117e0f6cc8c6bd9d9c0764f79cb24360029fc29
Nadiale04 <162038699+Nadiale04@users.noreply.github.com>
2024-04-07 05:02:20 +0000

    Solución

---
+++ Problema8/main.py
@@ -0,0 +1,9 @@
+import numerosaleatorios
+
+numeros_al = generador_numeros.generar_numeros()
+
+# Mostrar la lista generada por pantalla
+generador_numeros.mostrar_lista(numeros_aleatorios)
+
+# Ordenar y mostrar la lista por pantalla
+generador_numeros.ordenar_lista(numeros_aleatorios)
---
+++ Problema9/calculos.py
@@ -0,0 +1,9 @@
+import operaciones
+
+a=input('Ingrese un número: ')
+b=input('Ingrese otro número: ')
+
+print("Suma:", operaciones.suma(a,b))
+print("Resta:", operaciones.resta(a,b))
+print("Producto:", operaciones.producto(a,b))
+print("División:", operaciones.division(a,b))
